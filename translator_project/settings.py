41ec10b7d8f3e9fbd88b0608dab4abf9ffd48b08
alesanfe <alex.0002002@gmail.com>
2023-09-17 21:50:37 +0200

    :rocket: Configurado para que funcione en local.

--- translator_project/settings.py
+++ translator_project/settings.py
@@ -82,12 +82,20 @@ WSGI_APPLICATION = 'translator_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost/postgres',
-        conn_max_age=600
-    )
-}
+if not DEBUG:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default='postgresql://postgres:postgres@localhost/postgres',
+            conn_max_age=600
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
